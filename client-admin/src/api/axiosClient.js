ca7cd9dc1eb13d5b32ed40057b1bd7b38d07f02d
leebask <khanhkissyou123@gmail.com>
2022-12-06 10:27:00 +0700

    ss

--- client-admin/src/api/axiosClient.js
+++ client-admin/src/api/axiosClient.js
@@ -7,9 +7,7 @@ import queryString from 'query-string';
 
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'content-type': 'application/json',
-  },
+  headers: { 'content-type': "multipart/form-data" },
   paramsSerializer: (params) => queryString.stringify(params),
 });
 axiosClient.interceptors.request.use(async (config) => {
